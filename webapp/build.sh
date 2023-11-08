c3ba79409bdc479cd979fc91e029aace8d0a240c
Levi Mason <levmason@gmail.com>
2023-11-08 02:24:09 -0500

    remove dead code from build.sh

--- webapp/build.sh
+++ webapp/build.sh
@@ -33,23 +33,6 @@ cat ${files[@]} > css/flockpocket.dep.min.css
 #
 # flockpocket.min.js
 cd ../webapp/
-# files=(
-#     # list files here
-#     common/utility.js \
-#     common/modal.js \
-#     api/api.js \
-#     directory/badge.js \
-#     directory/profile.js \
-#     directory/directory.js \
-#     directory/results.js \
-#     directory/infocard.js \
-#     directory/familycard.js \
-#     form/input.js \
-#     form/new_user.js \
-#     menu/top_menu.js \
-#     menu/menu.js \
-#     flockpocket.js \
-# )
 files=(`find . -type f -name "*.js"`)
 if $QUICK; then
     command="cat"
@@ -60,18 +43,5 @@ $command ${files[@]} > ../static/js/flockpocket.min.js
 
 #
 # flockpocket.min.css
-# files=(
-#     # list files here
-#     common/modal.css \
-#     common/content.css \
-#     menu/menu.css \
-#     menu/top_menu.css \
-#     menu/left_menu.css \
-#     menu/right_menu.css \
-#     directory/directory.css \
-#     directory/profile.css \
-#     directory/badge.css \
-#     flockpocket.css
-# )
 files=(`find . -type f -name "*.css"`)
 cat ${files[@]} > ../static/css/flockpocket.min.css
