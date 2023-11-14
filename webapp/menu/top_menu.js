aea80d5017875e9382d543237f333af3156db155
Levi Mason <levmason@gmail.com>
2023-11-13 19:27:08 -0500

    Add topbar notification bell icon (draft)

--- webapp/menu/top_menu.js
+++ webapp/menu/top_menu.js
@@ -3,11 +3,18 @@ function top_menu (container, authorized = true) {
     el.call(self, container);
 
     self.logo_url = utility.static_url("img/logo.svg");
+    self.bell_url = utility.static_url("img/bell.svg");
     self.user_menu = null;
 
     // initialize
     self.init = function () {
-        self.html = `<div id="logo"><img src="${self.logo_url}"></div>`;
+        self.html = `
+          <div id="logo">
+            <img src="${self.logo_url}">
+          </div>
+          <div id="notifications">
+            <img src="${self.bell_url}">
+          </div>`;
 
         self.add_to_page();
         if (authorized) {
