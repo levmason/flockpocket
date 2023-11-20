75bc69095f92032966e438e30982d4f3b77f3489
Isaac Lyons <isaac@snowskeleton.net>
2023-11-20 17:17:58 -0500

    Simplify email because something about the more complex setup doesn't send properly

--- common/email.py
+++ common/email.py
@@ -8,34 +8,36 @@ from . import logger as log
 
 async def send (to, subject, message="", attachments=[]):
 
-    mailfrom = f"{cfg.tool_name} <noreply@flockpocket.com>"
+    mailfrom = f"{cfg.tool_name} <{cfg.email_from}>"
 
     # prepare the email
-    msg = EmailMessage()
-    msg['Subject'] = subject
-    msg['From'] = mailfrom
-    msg['To'] = ", ".join(to)
-    msg.set_content(message)
-    msg.add_alternative(f"<pre>{message}</pre>")
-    for attachment in attachments:
-        try:
-            name = attachment['name']
-            data = attachment['data']
+    # msg = EmailMessage()
+    # msg['Subject'] = subject
+    # msg['From'] = mailfrom
+    # msg['To'] = ", ".join(to)
+    # msg.set_content(message)
+    # msg.add_alternative(f"<pre>{message}</pre>")
+    # for attachment in attachments:
+    #     try:
+    #         name = attachment['name']
+    #         data = attachment['data']
 
-            # find the file type
-            ctype, encoding = mimetypes.guess_type(name)
-            if ctype is None or encoding is not None:
-                ctype = 'application/octet-stream'
-            maintype, subtype = ctype.split('/', 1)
-            msg.add_attachment(data.encode(),
-                               maintype=maintype,
-                               subtype=subtype,
-                               filename=name)
-        except Exception as e:
-            await log.error(f"Error sending email: {e}", "config")
-            return
+    #         # find the file type
+    #         ctype, encoding = mimetypes.guess_type(name)
+    #         if ctype is None or encoding is not None:
+    #             ctype = 'application/octet-stream'
+    #         maintype, subtype = ctype.split('/', 1)
+    #         msg.add_attachment(data.encode(),
+    #                            maintype=maintype,
+    #                            subtype=subtype,
+    #                            filename=name)
+    #     except Exception as e:
+    #         await log.error(f"Error sending email: {e}", "config")
+    #         return
 
+    message = '\n'.join([f"From: {mailfrom}", f"Subject: {subject}", message])
     # send the email
     with smtplib.SMTP_SSL(cfg.email_host, cfg.email_port, context=ssl.create_default_context()) as server:
-        server.login(cfg.email_host, cfg.email_password)
-        server.sendmail(cfg.email_host, to, message)
+        server.connect(f"{cfg.email_host}:{cfg.email_port}")
+        server.login(cfg.email_user, cfg.email_password)
+        server.sendmail(mailfrom, to, message)
